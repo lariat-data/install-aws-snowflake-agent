054929dab59b7b9e9d7a52dfc2494499bc7d0221
Aaditya Talwai <aaditya.21@gmail.com>
2022-10-17 18:59:49 +0100

    Lariat Snowflake Agent Installer

---
+++ install_snowflake_agent.py
@@ -0,0 +1,98 @@
+import os
+import json
+import subprocess
+import snowflake.connector
+
+
+SNOWFLAKE_WAREHOUSE = "COMPUTE_WH"
+SNOWFLAKE_ACCOUNT_LOCATOR="CL46984.us-east-2.aws"
+SNOWFLAKE_REGION="us-east-2.aws"
+SNOWFLAKE_ACCOUNT="CL46984"
+SNOWFLAKE_USER="aaditya"
+SNOWFLAKE_PASSWORD=os.environ.get("SNOWFLAKE_PASSWORD")
+SNOWFLAKE_DATABASE_ALLOWLIST = ["LARIAT_SNOWFLAKE_TEST"]
+
+LARIAT_API_KEY="162814e3ec2236e9e557ac8aa87a44cf"
+LARIAT_APPLICATION_KEY="28488da99d7842c6494aeaf4bf95251b"
+S3_QUERY_RESULTS_BUCKET="lariat-snowflak-default-query-results"
+S3_AGENT_CONFIG_BUCKET="lariat-snowflake-default-config"
+QUERY_DISPATCH_INTERVAL_CRON="cron(48 * * * ? *)"
+
+AWS_REGION="us-east-2"
+
+def get_snowflake_databases():
+    con = snowflake.connector.connect(
+            user=SNOWFLAKE_USER,
+            account=SNOWFLAKE_ACCOUNT_LOCATOR,
+            password=SNOWFLAKE_PASSWORD,
+            role="ACCOUNTADMIN",
+    )
+
+    cur = con.cursor()
+    result = cur.execute("SHOW DATABASES")
+    databases = [r[1] for r in result]
+
+    return databases
+
+def install_lariat_to_databases():
+    dbs = get_snowflake_databases()
+    filtered_dbs = [db for db in dbs if db.upper() in SNOWFLAKE_DATABASE_ALLOWLIST]
+    if not filtered_dbs:
+        print(f"No valid database found for Lariat installation")
+        return
+
+    print(f"Installing lariat to Snowflake Databases {filtered_dbs}")
+    tf_env = {
+        "TF_VAR_snowflake_account": SNOWFLAKE_ACCOUNT,
+        "TF_VAR_snowflake_default_warehouse": SNOWFLAKE_WAREHOUSE,
+        "TF_VAR_snowflake_user": SNOWFLAKE_USER,
+        "TF_VAR_snowflake_region": SNOWFLAKE_REGION,
+        "TF_VAR_snowflake_password": SNOWFLAKE_PASSWORD,
+        "TF_VAR_snowflake_databases": f'{json.dumps(filtered_dbs)}',
+        "TF_VAR_snowflake_account_locator": SNOWFLAKE_ACCOUNT_LOCATOR,
+
+        "TF_VAR_lariat_api_key": LARIAT_API_KEY,
+        "TF_VAR_lariat_application_key": LARIAT_APPLICATION_KEY,
+        "TF_VAR_s3_query_results_bucket": S3_QUERY_RESULTS_BUCKET,
+        "TF_VAR_s3_agent_config_bucket": S3_AGENT_CONFIG_BUCKET,
+        "TF_VAR_query_dispatch_interval_cron": QUERY_DISPATCH_INTERVAL_CRON,
+        "TF_VAR_aws_region": AWS_REGION,
+    }
+
+    my_env = os.environ.copy()
+    for k, v in tf_env.items(): my_env[k] = v
+
+    subprocess.run(["terraform", "apply"], env=my_env)
+
+def destroy_lariat_installations():
+    dbs = get_snowflake_databases()
+    filtered_dbs = [db for db in dbs if db.upper() in SNOWFLAKE_DATABASE_ALLOWLIST]
+    if not filtered_dbs:
+        print(f"No valid database found for Lariat installation")
+        return
+
+    print(f"Removing lariat from Snowflake Databases {filtered_dbs}")
+    tf_env = {
+        "TF_VAR_snowflake_account": SNOWFLAKE_ACCOUNT,
+        "TF_VAR_snowflake_default_warehouse": SNOWFLAKE_WAREHOUSE,
+        "TF_VAR_snowflake_user": SNOWFLAKE_USER,
+        "TF_VAR_snowflake_region": SNOWFLAKE_REGION,
+        "TF_VAR_snowflake_password": SNOWFLAKE_PASSWORD,
+        "TF_VAR_snowflake_databases": f'{json.dumps(filtered_dbs)}',
+        "TF_VAR_snowflake_account_locator": SNOWFLAKE_ACCOUNT_LOCATOR,
+
+        "TF_VAR_lariat_api_key": LARIAT_API_KEY,
+        "TF_VAR_lariat_application_key": LARIAT_APPLICATION_KEY,
+        "TF_VAR_s3_query_results_bucket": S3_QUERY_RESULTS_BUCKET,
+        "TF_VAR_s3_agent_config_bucket": S3_AGENT_CONFIG_BUCKET,
+        "TF_VAR_query_dispatch_interval_cron": QUERY_DISPATCH_INTERVAL_CRON,
+        "TF_VAR_aws_region": AWS_REGION,
+    }
+
+    my_env = os.environ.copy()
+    for k, v in tf_env.items(): my_env[k] = v
+
+    subprocess.run(["terraform", "destroy"], env=my_env)
+
+install_lariat_to_databases()
+#destroy_lariat_installations()
---
+++ snowflake.tf
@@ -0,0 +1,147 @@
+variable "snowflake_account" {
+  type = string
+}
+
+variable "snowflake_region" {
+  type = string
+}
+
+variable "snowflake_user" {
+  type = string
+}
+
+variable "snowflake_default_warehouse" {
+  type = string
+}
+
+variable "snowflake_password" {
+  type = string
+  sensitive = true
+}
+
+variable "snowflake_databases" {
+  type = list
+}
+
+variable "snowflake_account_locator" {
+  type = string
+}
+
+provider "snowsql" {
+  username = var.snowflake_user
+  password = var.snowflake_password
+  account = var.snowflake_account
+  region = var.snowflake_region
+  warehouse = var.snowflake_default_warehouse
+  role   = "accountadmin"
+}
+
+provider "snowflake" {
+  username = var.snowflake_user
+  password = var.snowflake_password
+  account = var.snowflake_account
+  region = var.snowflake_region
+  warehouse = var.snowflake_default_warehouse
+  role   = "accountadmin"
+}
+
+resource snowflake_warehouse "lariat_snowflake_warehouse" {
+  name           = "lariat_snowflake_warehouse"
+  comment        = "warehouse for running lariat monitoring queries"
+  warehouse_size = "xsmall"
+  initially_suspended = true
+  auto_suspend = 60 # suspend after 60 seconds of inactivity
+  auto_resume = true
+}
+
+resource snowflake_role "lariat_snowflake_role" {
+  name           = "lariat_snowflake_role"
+  comment        = "role for running lariat monitoring queries"
+}
+
+resource snowflake_warehouse_grant "lariat_snowflake_warehouse_grant_operate" {
+  warehouse_name = "${snowflake_warehouse.lariat_snowflake_warehouse.name}"
+  privilege = "OPERATE"
+
+  roles = [
+    "${snowflake_role.lariat_snowflake_role.name}"
+  ]
+
+  with_grant_option = false
+}
+
+resource snowflake_warehouse_grant "lariat_snowflake_warehouse_grant_usage" {
+  warehouse_name = "${snowflake_warehouse.lariat_snowflake_warehouse.name}"
+  privilege = "USAGE"
+
+  roles = [
+    "${snowflake_role.lariat_snowflake_role.name}"
+  ]
+
+  with_grant_option = false
+}
+
+resource snowflake_warehouse_grant "lariat_snowflake_warehouse_grant_monitor" {
+  warehouse_name = "${snowflake_warehouse.lariat_snowflake_warehouse.name}"
+  privilege = "MONITOR"
+
+  roles = [
+    "${snowflake_role.lariat_snowflake_role.name}"
+  ]
+
+  with_grant_option = false
+}
+
+resource "random_password" "lariat_snowflake_user_password" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
+resource snowflake_user "lariat_snowflake_user" {
+  name           = "lariat_snowflake_user"
+  password       = random_password.lariat_snowflake_user_password.result
+  comment        = "user for running lariat monitoring queries"
+
+  default_warehouse = snowflake_warehouse.lariat_snowflake_warehouse.name
+  default_role = snowflake_role.lariat_snowflake_role.name
+}
+
+resource snowflake_role_grants "lariat_snowflake_grants" {
+  role_name = "${snowflake_role.lariat_snowflake_role.name}"
+
+  users = [
+    "${snowflake_user.lariat_snowflake_user.name}"
+  ]
+}
+
+resource snowsql_exec "lariat_read_only_grants" {
+  for_each = toset(var.snowflake_databases)
+  name = "lariat-read-only-grants-${each.key}"
+  depends_on = [
+    snowflake_role_grants.lariat_snowflake_grants
+  ]
+
+  create {
+    statements = <<-EOT
+    GRANT USAGE ON DATABASE ${each.key} TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    GRANT USAGE ON ALL SCHEMAS IN DATABASE ${each.key} TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    GRANT SELECT ON ALL TABLES IN DATABASE ${each.key} TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    GRANT SELECT ON FUTURE TABLES IN DATABASE ${each.key} TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    EOT
+  }
+
+    delete {
+    statements = <<-EOT
+    REVOKE USAGE ON DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    REVOKE USAGE ON ALL SCHEMAS IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    REVOKE ALL PRIVILEGES ON ALL TABLES IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    REVOKE ALL PRIVILEGES ON FUTURE TABLES IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    EOT
+  }
+}
+
+output lariat_snowflake_user_password {
+  value     = random_password.lariat_snowflake_user_password.result
+  sensitive = true
+}
