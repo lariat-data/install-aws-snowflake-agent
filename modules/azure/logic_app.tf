bb1c7f7e3ffec732197a2866c11756ff0e49c3b7
Aaditya Talwai <aaditya.21@gmail.com>
2022-10-24 14:01:25 +0100

    Add registry name and password for docker azure link

---
+++ modules/azure/logic_app.tf
@@ -0,0 +1,70 @@
+# Contains app and recurrence definitions for an Azure Logic App,
+# an Azure service that we use to trigger the Lariat Monitoring Function App
+# at regular intervals
+
+resource "azurerm_logic_app_workflow" "lariat_monitoring_workflow_schema_pull" {
+  name                = "lariat-monitoring-workflow-schema-pull"
+  location            = azurerm_resource_group.lariat_resource_group.location
+  resource_group_name = azurerm_resource_group.lariat_resource_group.name
+}
+
+resource "azurerm_logic_app_action_custom" "lariat_raw_schema_action" {
+  name = "lariat-raw-schema-action"
+  logic_app_id = azurerm_logic_app_workflow.lariat_monitoring_workflow_schema_pull.id
+
+  body = <<BODY
+  {
+    "inputs": {
+        "body": {
+            "run_type": "raw_schema"
+        },
+        "function": {
+            "id": "${azurerm_linux_function_app.example.id}/functions/azure_snowflake_function"
+        }
+    },
+    "runAfter": {},
+    "type": "Function"
+  }
+  BODY
+}
+
+resource "azurerm_logic_app_trigger_recurrence" "lariat_daily_schema_pull" {
+  name         = "lariat-daily-schema-pull"
+  logic_app_id = azurerm_logic_app_workflow.lariat_monitoring_workflow_schema_pull.id
+  frequency    = "Day"
+  interval     = 1
+}
+
+
+resource "azurerm_logic_app_workflow" "lariat_monitoring_workflow_indicator_query" {
+  name                = "lariat-monitoring-workflow-indicator-query"
+  location            = azurerm_resource_group.lariat_resource_group.location
+  resource_group_name = azurerm_resource_group.lariat_resource_group.name
+}
+
+resource "azurerm_logic_app_action_custom" "lariat_indicator_query_action" {
+  name = "lariat-indicator-query-action"
+  logic_app_id = azurerm_logic_app_workflow.lariat_monitoring_workflow_indicator_query.id
+
+  body = <<BODY
+  {
+    "inputs": {
+        "body": {
+            "run_type": "batch_agent_query_dispatch"
+        },
+        "function": {
+            "id": "${azurerm_linux_function_app.example.id}/functions/azure_snowflake_function"
+        }
+    },
+    "runAfter": {},
+    "type": "Function"
+  }
+  BODY
+}
+
+resource "azurerm_logic_app_trigger_recurrence" "lariat_daily_indicator_query" {
+  name         = "lariat-frequent-indicator-query"
+  logic_app_id = azurerm_logic_app_workflow.lariat_monitoring_workflow_indicator_query.id
+  frequency    = "Minute"
+  interval     = 5
+}
--- snowflake.tf
+++ snowflake.tf
@@ -25,9 +25,33 @@ resource snowflake_warehouse "lariat_snowflake_warehouse" {
   auto_resume = true
 }
 
+resource snowflake_database "lariat_meta_database" {
+  name = "lariat_meta_db"
+}
+
+resource snowflake_schema "lariat_meta_db_schema" {
+  database = snowflake_database.lariat_meta_database.name
+  name     = "lariat"
+}
+
 resource snowflake_role "lariat_snowflake_role" {
   name           = "lariat_snowflake_role"
   comment        = "role for running lariat monitoring queries"
+}
+
+resource "snowflake_database_grant" "lariat_meta_db_grant" {
+  database_name = snowflake_database.lariat_meta_database.name
+
+  privilege = "USAGE"
+  roles     = [snowflake_role.lariat_snowflake_role.name]
+}
+
+resource "snowflake_schema_grant" "lariat_meta_schema_grant" {
+  database_name = snowflake_database.lariat_meta_database.name
+  schema_name   = snowflake_schema.lariat_meta_db_schema.name
+
+  privilege = "USAGE"
+  roles     = [snowflake_role.lariat_snowflake_role.name]
 }
 
 resource snowflake_warehouse_grant "lariat_snowflake_warehouse_grant_operate" {
@@ -108,6 +132,87 @@ resource snowsql_exec "lariat_read_only_grants" {
     REVOKE USAGE ON ALL SCHEMAS IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
     REVOKE ALL PRIVILEGES ON ALL TABLES IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
     REVOKE ALL PRIVILEGES ON FUTURE TABLES IN DATABASE ${each.key} FROM ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    EOT
+  }
+}
+
+resource snowsql_exec "lariat_snowflake_stage" {
+  name = "lariat-snowflake-stage"
+  depends_on = [
+    snowflake_schema.lariat_meta_db_schema
+  ]
+
+  create {
+    statements = <<-EOT
+    USE DATABASE "${snowflake_database.lariat_meta_database.name}";
+    USE SCHEMA "${snowflake_schema.lariat_meta_db_schema.name}";
+    CREATE OR REPLACE STAGE lariat_stage;
+
+    GRANT READ ON STAGE lariat_stage TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    EOT
+  }
+
+    delete {
+    statements = <<-EOT
+    USE DATABASE "${snowflake_database.lariat_meta_database.name}";
+    USE SCHEMA "${snowflake_schema.lariat_meta_db_schema.name}";
+    DROP STAGE IF EXISTS lariat_stage;
+    EOT
+  }
+}
+
+resource snowsql_exec "lariat_snowflake_sketch_udf_import" {
+  name = "lariat-snowflake-sketch-udf-import"
+  depends_on = [
+    snowsql_exec.lariat_snowflake_stage
+  ]
+
+  create {
+    statements = <<-EOT
+    put 'file://${path.cwd}/artifacts/java/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar' @"${snowflake_database.lariat_meta_database.name}"."${snowflake_schema.lariat_meta_db_schema.name}".lariat_stage auto_compress=false OVERWRITE=true;
+    EOT
+  }
+
+    delete {
+    statements = <<-EOT
+    USE DATABASE "${snowflake_database.lariat_meta_database.name}";
+    USE SCHEMA "${snowflake_schema.lariat_meta_db_schema.name}";
+    rm @lariat_stage pattern='.*agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar.*';
+    EOT
+  }
+}
+
+resource snowsql_exec "lariat_snowflake_sketch_udf_create" {
+  name = "lariat-snowflake-sketch-udf-function"
+  depends_on = [
+    snowsql_exec.lariat_snowflake_sketch_udf_import
+  ]
+
+  create {
+    statements = <<-EOT
+    USE DATABASE "${snowflake_database.lariat_meta_database.name}";
+    USE SCHEMA "${snowflake_schema.lariat_meta_db_schema.name}";
+    create or replace function hllpp_count_strings_sketch(x string)
+    returns table(sketch binary)
+    language java
+    imports = ('@lariat_stage/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar')
+    handler='com.lariat.agentudfs.HLPPCountStringsSketch';
+    create or replace function hll_merge(x array)
+    returns binary
+    language java
+    imports = ('@lariat_stage/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar')
+    handler='com.lariat.agentudfs.HLPPMerge.merge';
+    GRANT USAGE ON FUNCTION hllpp_count_strings_sketch(string) TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    GRANT USAGE ON FUNCTION hll_merge(array) TO ROLE "${lower(snowflake_role.lariat_snowflake_role.name)}";
+    EOT
+  }
+
+    delete {
+    statements = <<-EOT
+    USE DATABASE "${snowflake_database.lariat_meta_database.name}";
+    USE SCHEMA "${snowflake_schema.lariat_meta_db_schema.name}";
+    DROP FUNCTION IF EXISTS hllpp_count_strings_sketch(string);
+    DROP FUNCTION IF EXISTS hll_merge(array);
     EOT
   }
 }
