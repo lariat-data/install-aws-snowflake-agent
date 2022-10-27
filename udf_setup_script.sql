51b5fd6858f5a6fb3a466624d7e8c5bd2fc7a6bd
Vikas Shanbhogue <vikas.shanbhogue@gmail.com>
2022-10-27 21:39:01 +0200

    snowflake udf dependencies

---
+++ udf_setup_script.sql
@@ -0,0 +1,13 @@
+put 'file://artifacts/java/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar' @~ auto_compress=false OVERWRITE=true;
+
+create or replace function my_hllpp_count_strings_sketch(x string)
+returns table(sketch binary)
+language java
+imports = ('@~/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar')
+handler='com.lariat.agentudfs.HLPPCountStringsSketch';
+
+create or replace function my_hll_merge(x array)
+returns binary
+language java
+imports = ('@~/agent-udfs-0.1-SNAPSHOT-jar-with-dependencies.jar')
+handler='com.lariat.agentudfs.HLPPMerge.merge';
